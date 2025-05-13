457f14fa2e4d5a7ddbc064f48659f3c8455962c3
HetBalint <hetenyibalint06@gmail.com>
2025-05-13 12:29:12 +0200

    first commit

---
+++ Auto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Auto
+{
+    public int Id { get; set; }
+    public string Márka { get; set; }
+    public int Évjárat { get; set; }
+    public int Ár
+    {
+        get; set;
+    }
+
+    public Auto(int id, string márka, int évjárat, int ár)
+    {
+        Id = id;
+        Márka = márka;
+        Évjárat = évjárat;
+        Ár = ár;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace proba2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filenev = "C:\\Users\\heten\\Desktop\\proba2\\proba2\\autok.csv";
+            var autok = new List<Auto>();
+            string[] sorok = File.ReadAllLines(filenev);
+            foreach (var sor in sorok.Skip(1))
+            {
+                var tagok = sor.Split(',');
+                if (tagok.Length == 4 )
+                {
+                    autok.Add(new Auto(int.Parse(tagok[0]), tagok[1], int.Parse(tagok[2]), int.Parse(tagok[3]) ));
+                }
+            }
+            foreach (var auto in autok)
+            {
+                Console.WriteLine($"{auto.Id} {auto.Évjárat} {auto.Ár} {auto.Márka}");
+                
+                
+            }
+            
+            Auto legfiatalabb = autok.OrderBy(a => a.Évjárat).Last();
+            Auto legidősebb = autok.OrderBy(a => a.Évjárat).First();
+            Console.WriteLine($"Legidősebb:{legidősebb.Évjárat}");
+            Console.WriteLine($"Legfiatalabb:{legfiatalabb.Évjárat}");
+            int km = 100;
+            int üzár = 560;
+            int fogy = 6;
+            költés(km,üzár, fogy);
+        }
+
+
+
+        public static void költés(int km, int üzár, int fogy)
+        {
+            int összliter = (km * fogy) / 100;
+            int költség = összliter * üzár;
+
+            Console.WriteLine($"Várható üzemanyag költség: {költség}");
+
+        }
+    }
+
+}
