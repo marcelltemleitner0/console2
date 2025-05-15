4ae4a8c76bed21d8eee350a1550180defef5d142
marcelltemleitner0 <marcelltemleitner@gmail.com>
2025-05-15 14:00:56 +0200

    group by

--- Program.cs
+++ Program.cs
@@ -1,27 +1,50 @@
-﻿namespace proba2
+namespace proba2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string filenev = "C:\\Users\\heten\\Desktop\\proba2\\proba2\\autok.csv";
+            string filenev = "autok.csv";
             var autok = new List<Auto>();
             string[] sorok = File.ReadAllLines(filenev);
             foreach (var sor in sorok.Skip(1))
             {
                 var tagok = sor.Split(',');
-                if (tagok.Length == 4 )
+                if (tagok.Length == 4)
                 {
-                    autok.Add(new Auto(int.Parse(tagok[0]), tagok[1], int.Parse(tagok[2]), int.Parse(tagok[3]) ));
+                    autok.Add(new Auto(int.Parse(tagok[0]), tagok[1], int.Parse(tagok[2]), int.Parse(tagok[3])));
                 }
             }
             foreach (var auto in autok)
             {
                 Console.WriteLine($"{auto.Id} {auto.Évjárat} {auto.Ár} {auto.Márka}");
-                
-                
+
+
             }
-            
+
+            var evjaratAtlagAr = autok
+                .Where(a => a.Évjárat == 2015)
+                .GroupBy(a => a.Évjárat)
+                .Select(g => new
+                {
+                    Évjárat = g.Key,
+                    ÁtlagÁr = g.Average(a => a.Ár)
+                })
+                .OrderBy(g => g.Évjárat);
+
+            Console.WriteLine("\nÁtlag ár évjáratonként:");
+            foreach (var item in evjaratAtlagAr)
+            {
+                Console.WriteLine($"Évjárat: {item.Évjárat}, Átlag ár: {item.ÁtlagÁr:N0} Ft");
+            }
+
+
+
+
+
+
+
+
             Auto legfiatalabb = autok.OrderBy(a => a.Évjárat).Last();
             Auto legidősebb = autok.OrderBy(a => a.Évjárat).First();
             Console.WriteLine($"Legidősebb:{legidősebb.Évjárat}");
@@ -29,7 +52,7 @@
             int km = 100;
             int üzár = 560;
             int fogy = 6;
-            költés(km,üzár, fogy);
+            költés(km, üzár, fogy);
         }
 
 
